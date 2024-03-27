dddc70f0712134b357866f7bf14e06ef6f36621e
Alexandre Da Silva <alexandrerider@hotmail.com>
2024-03-27 19:41:18 -0300

    Primeiro comite

---
+++ src/main/java/com/fate/GrimoireOfHeartApi/GrimoireOfHeartApiApplication.java
@@ -0,0 +1,13 @@
+package com.fate.GrimoireOfHeartApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrimoireOfHeartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrimoireOfHeartApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fate/GrimoireOfHeartApi/GrimoireOfHeartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fate.GrimoireOfHeartApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrimoireOfHeartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
